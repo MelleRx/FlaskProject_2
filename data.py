5e051f46f1446e23b1f206b2c6cc8c0791ce1558
MelleRx <kukulidi@yandex.ru>
2020-11-14 22:00:20 +0300

    add goal - programming

--- data.py
+++ data.py
@@ -272,7 +272,7 @@ teachers = [
         "picture": "https://i.pravatar.cc/300?img=35",
         "rating": 5,
         "price": 1700,
-        "goals": ["relocate", "work"],
+        "goals": ["relocate", "work", "programming"],
         "free": {
 
             "mon": {"8:00": False, "10:00": False, "12:00": False, "14:00": False, "16:00": True, "18:00": True,
@@ -309,7 +309,7 @@ teachers = [
         "picture": "https://i.pravatar.cc/300?img=36",
         "rating": 4.1,
         "price": 1200,
-        "goals": ["work"],
+        "goals": ["work", "programming"],
         "free": {
 
             "mon": {"8:00": False, "10:00": False, "12:00": False, "14:00": False, "16:00": True, "18:00": True,
@@ -343,7 +343,7 @@ teachers = [
         "picture": "https://i.pravatar.cc/300?img=37",
         "rating": 4.7,
         "price": 1100,
-        "goals": ["travel", "study", "work"],
+        "goals": ["travel", "study", "work", "programming"],
         "free": {
 
             "mon": {"8:00": False, "10:00": False, "12:00": False, "14:00": False, "16:00": True, "18:00": True,
@@ -376,7 +376,7 @@ teachers = [
         "picture": "https://i.pravatar.cc/300?img=38",
         "rating": 4.2,
         "price": 900,
-        "goals": ["travel", "work"],
+        "goals": ["travel", "work", "programming"],
         "free": {
 
             "mon": {"8:00": False, "10:00": False, "12:00": False, "14:00": False, "16:00": True, "18:00": True,
